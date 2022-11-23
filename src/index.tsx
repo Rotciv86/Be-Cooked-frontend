c43a58a0bdb532cae015889135cd1502e803e9d5
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-23 10:39:47 +0100

    Place in correct folders App.tsx and hooks.ts and store.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
+import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import { store } from "./redux/store";
+import { store } from "./redux/features/store";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
