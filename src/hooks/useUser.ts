c3f598808ecb5da9974c3b49a0f7839d35cf4a7e
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-23 13:06:06 +0100

    Install axios package and add try and catch in register user method

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,14 +1,17 @@
+import axios from "axios";
 import { UserRegisterData } from "../types/types";
 
 const useUser = () => {
   const apiUrl = process.env.REACT_APP_API_URL!;
 
   const registerUser = async (userData: UserRegisterData) => {
-    await fetch(`${apiUrl}/users/sign-up`, {
-      method: "POST",
-      body: JSON.stringify(userData),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
-    });
+    try {
+      await axios.post(`${apiUrl}/users/sign-up`, userData);
+    } catch (error: unknown) {
+      throw new Error(
+        `It was not possible to register: ${(error as Error).message}`
+      );
+    }
   };
 
   return { registerUser };
