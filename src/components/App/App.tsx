f4d2a0be09db06190db244de36d93394e61d7abc
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-25 16:05:27 +0100

    Test RegisterForm own elements to be in the document when it rendered

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import RegisterForm from "../RegisterForm/RegisterForm";
 
 const App = () => {
   return (
     <div className="container">
       <h1>Be Cooked</h1>
-      <RegisterForm />
     </div>
   );
 };
