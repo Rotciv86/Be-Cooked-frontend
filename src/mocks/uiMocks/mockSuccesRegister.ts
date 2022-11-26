f28a3497e3a16e061b960d62c3c4ce97f7ddb75f
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-26 09:16:07 +0100

    Test Feedback ui component with success case

---
+++ src/mocks/uiMocks/mockSuccesRegister.ts
@@ -0,0 +1,12 @@
+import { UiState } from "../../types/types";
+
+const mockSuccessRegister: UiState = {
+  feedback: {
+    isError: false,
+    isOpen: true,
+    messageFeedback: "¡Bienvenido a Be Cooked!",
+  },
+  isLoading: false,
+};
+
+export default mockSuccessRegister;
