d3a9af5cffef035f4671bc8de6a10bde87772d47
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-26 18:09:25 +0100

    Create Layout ui component

--- src/index.tsx
+++ src/index.tsx
@@ -7,18 +7,21 @@ import { store } from "./redux/store";
 import GlobalStyles from "./styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import { mainTheme } from "./styles/mainTheme";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={mainTheme}>
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyles />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
