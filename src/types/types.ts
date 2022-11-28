b4eefbc472b9b1c2d8d5b74ba0b22cc9975bb007
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-28 13:44:08 +0100

    Create loginUser reducer and login action creator

--- src/types/types.ts
+++ src/types/types.ts
@@ -16,3 +16,13 @@ export interface OpenFeedbackActionPayload {
   messageFeedback: string;
   isError: boolean;
 }
+
+export interface UserLoginData {
+  username: string;
+  id: string;
+  token: string;
+}
+
+export interface UserState extends UserLoginData {
+  isLogged: boolean;
+}
