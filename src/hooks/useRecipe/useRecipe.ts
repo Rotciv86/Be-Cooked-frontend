dbf0219b98d3b3abb57b2df64206f8de18f5b623
Víctor Merino <victor.cornella.86@gmail.com>
2022-12-02 16:55:45 +0100

    Add delete Recipe method to recipes custom hook

--- src/hooks/useRecipe/useRecipe.ts
+++ src/hooks/useRecipe/useRecipe.ts
@@ -1,8 +1,12 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useCallback } from "react";
-import { loadAllRecipesActionCreator } from "../../redux/features/recipeSlice/recipeSlice";
+import {
+  deleteRecipeActionCreator,
+  loadAllRecipesActionCreator,
+} from "../../redux/features/recipeSlice/recipeSlice";
 import { openFeedbackActionCreator } from "../../redux/features/uiSlice/uiSlice";
 import { useAppDispatch } from "../../redux/hooks";
+import { AxiosResponseBody } from "../types";
 
 const useRecipe = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
@@ -27,7 +31,28 @@ const useRecipe = () => {
     }
   }, [dispatch, apiUrl]);
 
-  return { loadAllRecipes };
+  const deleteRecipe = async (recipeId: string) => {
+    try {
+      await axios.delete(`${apiUrl}/recipes/delete/${recipeId}`, {});
+      dispatch(deleteRecipeActionCreator(recipeId));
+      dispatch(
+        openFeedbackActionCreator({
+          isError: false,
+          messageFeedback: "Your recipe was successfully deleted",
+        })
+      );
+    } catch (error: unknown) {
+      dispatch(
+        openFeedbackActionCreator({
+          isError: true,
+          messageFeedback: (error as AxiosError<AxiosResponseBody>).response
+            ?.data.error!,
+        })
+      );
+    }
+  };
+
+  return { loadAllRecipes, deleteRecipe };
 };
 
 export default useRecipe;
