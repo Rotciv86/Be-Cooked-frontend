b59cda19caa8951c29512b75c0a3267c739496ae
Víctor Merino <victor.cornella.86@gmail.com>
2022-12-02 19:40:42 +0100

    Refactor RecipeCard component test to test its delete buton action

--- src/components/RecipeCard/RecipeCard.test.tsx
+++ src/components/RecipeCard/RecipeCard.test.tsx
@@ -1,7 +1,15 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { mockRecipes } from "../../mocks/recipeMocks/mockRecipe";
 import renderWithProviders from "../../utils/testUtils/renderWithProviders";
 import RecipeCard from "./RecipeCard";
+
+const mockDeleteRecipe = jest.fn();
+jest.mock("../../hooks/useRecipe/useRecipe", () => {
+  return () => ({
+    deleteRecipe: mockDeleteRecipe,
+  });
+});
 
 describe("Given the RecipeCard ui component", () => {
   describe("When it is rendered", () => {
@@ -22,6 +30,23 @@ describe("Given the RecipeCard ui component", () => {
       });
 
       expect(recipeCard).toHaveTextContent(recipeName);
+    });
+
+    test("Then it should call the action when clicked on the 'BORRAR' button", async () => {
+      renderWithProviders(
+        <RecipeCard
+          image={mockRecipes[0].image}
+          name={mockRecipes[0].name}
+          id={mockRecipes[0].id!}
+        />
+      );
+
+      const button = screen.queryByRole("button", {
+        name: "BORRAR",
+      });
+      await userEvent.click(button!);
+
+      expect(mockDeleteRecipe).toHaveBeenCalled();
     });
   });
 });
