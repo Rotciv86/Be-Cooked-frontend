b519534468c644e24eedbb96f7bd906b1b74b6fc
Víctor Merino <victor.cornella.86@gmail.com>
2022-12-07 10:48:54 +0100

    Add conditional rendering with login state to create recipe link

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,13 +1,22 @@
 import { screen } from "@testing-library/react";
+import recipeInitialStateMock from "../../mocks/recipeMocks/recipeInitialState";
+import mockSuccessRegister from "../../mocks/uiMocks/mockSuccesRegister";
+import userInitialStateMock from "../../mocks/userMocks/userInitialStateMock";
 import renderWithProviders from "../../utils/testUtils/renderWithProviders";
 import HomePage from "./HomePage";
 
 describe("Given the HomePage", () => {
-  describe("When is rendered", () => {
+  describe("When is rendered and the user is not logged", () => {
     test("Then it should show a heading level 1 with text 'Prepara tu propia receta...'", () => {
       const headingText = "Prepara tu propia receta...";
 
-      renderWithProviders(<HomePage />);
+      renderWithProviders(<HomePage />, {
+        preloadedState: {
+          ui: mockSuccessRegister,
+          user: userInitialStateMock,
+          recipes: recipeInitialStateMock,
+        },
+      });
 
       const heading = screen.queryByRole("heading", {
         level: 1,
@@ -16,6 +25,31 @@ describe("Given the HomePage", () => {
 
       expect(heading).toBeInTheDocument();
       expect(heading).toHaveTextContent(headingText);
+    });
+  });
+
+  describe("When is rendered and the user is logged", () => {
+    test("Then it should show a heading level 1 with text 'Prepara tu propia receta...' a button with the text 'Crea tu receta'", () => {
+      const headingText = "Prepara tu propia receta...";
+      const linkText = "Crea tu receta";
+      renderWithProviders(<HomePage />, {
+        preloadedState: {
+          ui: mockSuccessRegister,
+          user: { ...userInitialStateMock, isLogged: true },
+          recipes: recipeInitialStateMock,
+        },
+      });
+
+      const heading = screen.queryByRole("heading", {
+        level: 1,
+        name: headingText,
+      });
+      const link = screen.queryByRole("link", { name: linkText });
+
+      expect(heading).toBeInTheDocument();
+      expect(heading).toHaveTextContent(headingText);
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveTextContent(linkText);
     });
   });
 });
