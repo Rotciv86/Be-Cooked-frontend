ee454e0e3663760f5b6ccaa01862492dc6b93277
Víctor Merino <victor.cornella.86@gmail.com>
2022-12-07 12:26:13 +0100

    Add unitary test to delete button conditional rendering when logged

--- src/components/RecipeCard/RecipeCard.test.tsx
+++ src/components/RecipeCard/RecipeCard.test.tsx
@@ -1,6 +1,9 @@
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { mockRecipes } from "../../mocks/recipeMocks/mockRecipe";
+import recipeInitialStateMock from "../../mocks/recipeMocks/recipeInitialState";
+import mockSuccessRegister from "../../mocks/uiMocks/mockSuccesRegister";
+import userInitialStateMock from "../../mocks/userMocks/userInitialStateMock";
 import renderWithProviders from "../../utils/testUtils/renderWithProviders";
 import RecipeCard from "./RecipeCard";
 
@@ -31,14 +34,23 @@ describe("Given the RecipeCard ui component", () => {
 
       expect(recipeCard).toHaveTextContent(recipeName);
     });
+  });
 
+  describe("When it is rendered and the user is logged", () => {
     test("Then it should call the action when clicked on the 'BORRAR' button", async () => {
       renderWithProviders(
         <RecipeCard
           image={mockRecipes[0].image}
           name={mockRecipes[0].name}
           id={mockRecipes[0].id!}
-        />
+        />,
+        {
+          preloadedState: {
+            ui: mockSuccessRegister,
+            user: { ...userInitialStateMock, isLogged: true },
+            recipes: recipeInitialStateMock,
+          },
+        }
       );
 
       const button = screen.queryByRole("button", {
