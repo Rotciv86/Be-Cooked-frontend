c122e4e4d3c3b9c00343c71cc32cb1d216112bca
Víctor Merino <victor.cornella.86@gmail.com>
2023-01-19 13:15:18 +0100

    Add JwtCustomPayload as interface

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -1,3 +1,10 @@
+import { JwtPayload } from "jwt-decode";
+
 export interface AxiosResponseBody {
   error: string;
+}
+
+export interface JwtCustomPayload extends JwtPayload {
+  id: string;
+  username: string;
 }
